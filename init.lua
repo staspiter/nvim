0f63738b0d6a6175cd8cd5f05184d81cba00512c
Stas Piter <stas@forji.org>
2024-11-30 21:57:08 +0000

    Add reselect pasted key map

--- init.lua
+++ init.lua
@@ -104,6 +104,8 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', 'gp', '`[v`]', { desc = 'Re-select last pasted text' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
